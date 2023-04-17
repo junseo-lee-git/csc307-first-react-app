b589ff5d8c834515f54969501cb2d295e00ae563
Junseo Lee <junseoleee@gmail.com>
2023-04-16 23:37:50 -0700

    added DELETE functionality and name/job queries

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -36,23 +36,51 @@ const users = {
 
 app.use(express.json());
 
+// Hellow World!
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+
+// POST functionality for adding users. No error handling yet
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
-    res.status(200).end();
+    res.status(201).end();
 });
 
 function addUser(user){
     users['users_list'].push(user);
 }
 
+// DELETE functinality
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        deleteUser(result);
+        res.send(users);
+    }
+});
+
+function deleteUser(user){
+    idx = users['users_list'].indexOf(user);
+    users['users_list'].splice(idx, 1);
+}
+
+
+// GET functionality, returns either full list or specific queries
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    const job = req.query.job;
+    if (name != undefined && job != undefined){
+        let result = findUserByNameAndJob(name, job);
+        result = {users_list: result};
+        res.send(result);
+    }
+    if (name != undefined && job == undefined){
         let result = findUserByName(name);
         result = {users_list: result};
         res.send(result);
@@ -62,6 +90,16 @@ app.get('/users', (req, res) => {
     }
 });
 
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByNameAndJob = (name, job) => { 
+    return users['users_list'].filter( (user) => user['name'] === name && user['job'] === job); 
+}
+
+
+// Finds users by ID
 app.get('/users/:id', (req, res) => {
     const id = req.params['id']; //or req.params.id
     let result = findUserById(id);
@@ -78,9 +116,7 @@ function findUserById(id) {
     //return users['users_list'].filter( (user) => user['id'] === id);
 }
 
-const findUserByName = (name) => { 
-    return users['users_list'].filter( (user) => user['name'] === name); 
-}
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
